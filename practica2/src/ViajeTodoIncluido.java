ac0f935abd7b8abdf9708db642090998afc1bfb4
Brayan <bsagrcia@unicauca.edu.co>
2019-04-24 15:27:11 -0500

    commit incial

---
+++ practica2/src/ViajeTodoIncluido.java
@@ -0,0 +1,26 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author INGESIS
+ */
+public class ViajeTodoIncluido extends Viaje {
+
+    public ViajeTodoIncluido(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada) {
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+    }
+
+    @Override
+    public String descripcion() {
+        return "Disfruta tu viaje todo incluido";
+    }
+    
+    
+}
